ca7fe3c9b77123cfd9e756b485c1bd8378ab2057
suthyscott <sutherlandscott21@gmail.com>
2023-08-15 11:49:15 -0600

    how to use post requests

--- app.js
+++ app.js
@@ -8,10 +8,25 @@ const port = 8000
 // Need this to replace __dirname because we're using js modules here (see package.json line 2)
 const rootDir = url.fileURLToPath(new URL('.', import.meta.url));
 
+app.use(express.urlencoded({ extended: false }));
+
 
 app.get('/', (req, res) => {
     // path.join smartly joins the the roodDir (file path through the computer to this point) and the path to the file we're trying to render.
-    res.sendFile(path.join(rootDir, './views/home.html'))
+    res.sendFile(path.join(rootDir, './views/form.html'))
 })
+
+app.post('/fav-number', (req, res) => {
+    const favNumber = req.body.favNumber;
+    console.log('Saving favorite number to the database...');
+  
+    res.send(`Your favorite number is ${favNumber}.`);
+  });
+
+  app.post('/welcome', (req, res) => {
+    const person = req.body.person;
+    res.send(`Welcome, ${person}!`);
+  });
+  
 
 app.listen(port, () => console.log(`Take me to warp ${port}`))
